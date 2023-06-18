66e4497320643ea12f3c0fdae2810f095cd23d40
DIS686 <maksimdis31@gmail.com>
2023-06-18 11:23:44 +0300

    dwn HM4 task2

--- Homework#4/task2.py
+++ Homework#4/task2.py
@@ -2,32 +2,29 @@
 # где ключ — значение переданного аргумента, а значение — имя аргумента. Если ключ не хешируем,
 # используйте его строковое представление.
 
-import hashlib
+# Изменяемые объекты
+list_1 = (6, 7)  # кортеж
+dict_ = {6: 7, 7: 2}  # кортеж
+set_ = set[1, 2, 3, 4, 5]
+int_ = 1, 2, 3, 45
 
-
-# items = {"палатка": 15, "нож": 0.6, "стол": 10, "стул": 5, "спальный мешок": 1.5, "Топор": 2, "продукты": 2}
-
-
-# def tt(arg):  # возвращает ключи
-#     return arg.keys()
-#
-#
-# key = tt(items)
-# print(key)
-#
-
-# def task_2(**kwargs):
-#     for key, value in kwargs.items():
-#         return f' ключ {key} значение {value}'
-#
 
 def task_2_2(**kwargs):
     result = {}
     for key, value in kwargs.items():
-        if not hash(key):  # не знаю правильный ли подход
+        if not hash(key):
             key = str(key)
         result[key] = value
     return result
 
 
-print(task_2_2(key=1, valie='hhghjj'))
+print(task_2_2(key=list_1, valie='hhghjj'))
+print(task_2_2(key=dict_, valie='hhghjj'))
+print(task_2_2(key=set_, valie='hhghjj'))
+print(task_2_2(key=int_, valie='hhghjj'))
+
+if __name__ == " __main__":
+    print(task_2_2(key=list_1, valie='hhghjj'))
+    print(task_2_2(key=dict_, valie='hhghjj'))
+    print(task_2_2(key=set_, valie='hhghjj'))
+    print(task_2_2(key=int_, valie='hhghjj'))
